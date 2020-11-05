801542f7e0c0afb1c2fec13ce7a6c0d6120ca55e
Chris Spohn <chris.j.spohn@gmail.com>
2020-11-05 16:18:04 -0700

    Add winner screen update in DOM

--- src/game.js
+++ src/game.js
@@ -4,7 +4,8 @@ class Game {
     this.turn = this.players[0];
     this.board = [[1],[2],[3],
                   [4],[5],[6],
-                  [7],[8],[9]]
+                  [7],[8],[9]];
+    this.winner = ''
   }
 
   changeTurn() {
@@ -26,6 +27,7 @@ class Game {
   checkRows() {
     for (var i = 0; i < 9; i++) {
       if (this.board[i][0] === this.board[i+1][0] && this.board[i][0] === this.board[i+2][0]) {
+        this.winner = this.board[i][0]
         return true
       }
       i += 2
@@ -35,14 +37,18 @@ class Game {
   checkCols() {
     for (var i = 0; i < 3; i++) {
       if (this.board[i][0] === this.board[i+3][0] && this.board[i][0] === this.board[i+6][0]) {
+        this.winner = this.board[i][0]
         return true
       }
     }
   }
 
   checkDiags() {
-    if ((this.board[0][0] === this.board[4][0] && this.board[0][0] === this.board[8][0]) ||
-       (this.board[2][0] === this.board[4][0] && this.board[2][0] === this.board[6][0])) {
+    if (this.board[0][0] === this.board[4][0] && this.board[0][0] === this.board[8][0]) {
+      this.winner = this.board[0][0];
+      return true
+    } else if (this.board[2][0] === this.board[4][0] && this.board[2][0] === this.board[6][0]) {
+      this.winner = this.board[2][0]
       return true
     }
   }
@@ -53,6 +59,7 @@ class Game {
   }
 
   resetGameBoard() {
-    
+    turnDisplay.innerText = 'won!'
+    turnDisplay.insertAdjacentHTML('afterbegin', this.turn.tokenId)
   }
 }
